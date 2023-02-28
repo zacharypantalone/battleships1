d3aa462cdcc3abdb6572e0366f00019e7b8d6ca7
Zachary Pantalone <zachary.pantalone@gmail.com>
2023-02-28 15:09:23 -0500

    grids added, see notes in styles.css

--- app.js
+++ app.js
@@ -31,6 +31,13 @@ function createBoard(color, user) {
   gameBoardContainer.classList.add('game-board');
   gameBoardContainer.style.backgroundColor = color;
   gameBoardContainer.id = user;
+
+  for (let i = 0; i < width * width; i++) {
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.id = i;
+    gameBoardContainer.append(block)
+  }
   gamesBoardContainer.append(gameBoardContainer);
 }
 
