c1d5c4ef59823276a2f30303e91b36d4d46cb25e
Zachary Pantalone <zachary.pantalone@gmail.com>
2023-04-13 10:51:14 -0400

    changed randomStartIndex in vairable validStart to startIndex

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ function addShipPiece(user, ship, startId) {
 
   let startIndex = startId ? startId : randomStartIndex;
   
-  let validStart = isHorizontal ? randomStartIndex <= width * width - ship.length ? randomStartIndex : width * width - ship.length : randomStartIndex <= width * width - width * ship.length ? randomStartIndex : randomStartIndex -ship.length * width + width;
+  let validStart = isHorizontal ? startIndex <= width * width - ship.length ? startIndex : width * width - ship.length : startIndex <= width * width - width * ship.length ? startIndex : startIndex -ship.length * width + width;
   let shipBlocks = [];
 
 
