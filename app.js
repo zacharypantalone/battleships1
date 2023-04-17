3498ceac71593e05eae2622b3d3e0a5fa102c593
Zachary Pantalone <zachary.pantalone@gmail.com>
2023-04-17 17:51:13 -0400

    querSelectorAll object error corrected

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ function addShipPiece(user, ship, startId) {
         shipBlock.classList.add('taken');
       })
     } else {
-      if (user === 'computer') addShipPiece(ship)
+      if (user === 'computer') addShipPiece(user, ship, startId)
       if (user === 'player') notDropped = true
     }
 
